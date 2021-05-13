1d03fc0606e6fc2e7b0c5d26a074fc4023c943d0
LBU <noeliden1@gmail.com>
2021-05-13 01:33:48 -0400

    todo-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef, useCallback, useReducer } from 'react';
+import TodoTemplate from './todo-app-components/TodoTemplate'
+import TodoInsert from './todo-app-components/TodoInsert'
+import TodoList from './todo-app-components/TodoList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `todo ${i}`,
+      checked: false,
+    })
+  }
+  return array;
 }
+
+const App = () => {
+  // const [todos, setTodos] = useState(createBulkTodos)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: 'Learn Sass',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: 'Learn Javascript',
+      checked: true,
+    },
+    {
+      id: 3,
+      text: 'Learn React',
+      checked: false
+    }
+  ]);
+
+  // 고유값으로 사용될 id
+  // ref를 사용하여 변수 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      }
+      setTodos(todos => todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  )
+
+  const onRemove = useCallback(id => {
+    setTodos(todos => todos.filter(todo => todo.id !== id))
+  }, [todos])
+
+  const onToggle = useCallback(id => {
+    setTodos(todos => todos.map(todo => todo.id === id ? { ...todo, checked: !todo.checked } : todo))
+  }, [todos])
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+    </TodoTemplate>
+  )
+};
 
 export default App;
